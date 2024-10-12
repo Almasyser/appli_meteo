68ba6462f8b2424eb2a7bd342ff3357bedb82af5
Almasyser <almasyser@wanadoo.fr>
2024-10-12 16:17:47 +0200

    selecteur meteoData en cours. integrer le comp au code

--- src/frontend/components/compose-url/Compose-url.jsx
+++ src/frontend/components/compose-url/Compose-url.jsx
@@ -1,16 +1,40 @@
 import "./compose-url.css";
 import options from "../../assets/options.json";
+import { useState } from "react";
+
 function Compose_url() {
-  console.log(options);
-  
+  const lat=42.12356;
+  const long=6.32511;
+  const [urlFinal, setUrlFinal ]= useState(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`);
+  const handleClick = ()=>{
+    console.log("Validé!");
+  }
+  const handleChange = (e)=>{
+    const el = e.target;
+    if(el.checked){
+      setUrlFinal(urlFinal+el.id);
+    } else {
+      setUrlFinal(urlFinal.replace(el.id,''));
+    }
+  }
   return(
-    <menu className="menu-container">
+    <>
+    <p className="title-label">Sélectionner les valeurs à interroger:</p>
+    <ul className="menu-container" >
       {options && options.map((el)=>{
         return(
-          <li key={el.id}>{el.label}</li>
+          <li key={el.id} className="menu-line">
+            <input type="checkbox" id={el.name} checked={el.checked} disabled={el.disabled} onChange={handleChange}/>
+            <label htmlFor={el.name}>{el.label}</label>
+          </li>
         )
       })}
-    </menu>
+    </ul>
+      <section className="btn-container">
+        <p className="title-label">Valider cette URL:</p>
+        <button type="button" id="valide" className="btn-envoyer" onClick={handleClick}>{urlFinal}</button>
+    </section>
+    </>
   )
 }
 export default Compose_url;
