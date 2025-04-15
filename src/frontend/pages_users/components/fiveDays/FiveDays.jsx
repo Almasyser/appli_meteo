10bf30b9dc95544488db9059d304eee48c205f7b
Almasyser <almasyser@wanadoo.fr>
2025-04-16 00:34:41 +0200

    peupler le tableau fivDays

--- src/frontend/pages_users/components/fiveDays/FiveDays.jsx
+++ src/frontend/pages_users/components/fiveDays/FiveDays.jsx
@@ -1,12 +1,32 @@
 import PropTypes from "prop-types";
+import "./fivedays.css";
+import data from "./data.json";
 function ModalFiveDays(props){
   const { toggleFiveDays, setToggleFiveDays} = props;
+  console.log(data[4]);
+  
   return(
-    <div className="fiveDays-grid-container">
+    <>
+      <div className="fiveDays-grid-container">
+        <table>
 
+          <tbody>
+            {data.map((row, i) => (
+            <div key={i} className={(i==0)? "entete noHover":"entete"} htmlFor="head" >
+              {Object.values(row).map((cell, j) => (
+                <td key={j}>
+                  {cell.img && <img src={cell.img} alt="" id="head"/>}
+                  <span htmlFor="head" >{`${cell.text}${cell.unit}`}</span>
+                </td>
+              ))}
+            </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <h3>FiveDays</h3>
       <button type="button" onClick={()=> setToggleFiveDays(!toggleFiveDays)}>Five Days</button>
-    </div>
+    </>
   )
 }
 ModalFiveDays.propTypes = {
