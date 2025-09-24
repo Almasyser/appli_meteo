bdf839cc9a07560f2558ec49fea4f71aa3030aa4
Almasyser <almasyser@wanadoo.fr>
2025-09-24 21:34:55 +0200

    factoriser scss

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -4,16 +4,11 @@ import ModalLocation from "../components/location/Location";
 import ModalWeather from "../components/weather/Weather";
 import ModalComments from "../components/comments/Comments";
 import ModalFooter from "../components/footer/Footer";
-import CheckPosition from "../components/utils/CheckPosition";
+// import CheckPosition from "../components/utils/CheckPosition";
 import "./home.css";
 function Home () {
-  const [userLocation, setUserLocation]=useState(null);
-  const handleClick=()=>{
-    const coordonnees = <CheckPosition userLocation={userLocation} setUserLocation={setUserLocation}/>;
-    // console.log(coordonnees);
-    
-  }
-  
+  const [userLocation, setUserLocation]=useState({});
+  const [showPosition, setShowPosition] = useState(false);
   return (
     <>
       <div className="body">
@@ -28,7 +23,13 @@ function Home () {
           <ModalComments />
         </>      
         <ModalFooter />
-        <button onClick={handleClick}>Position</button>
+        {/* <button onClick={()=>setShowPosition(!showPosition)}>Position</button>
+        { showPosition && 
+        <>
+          <CheckPosition setUserLocation={setUserLocation}/> 
+          <h4>latitude {userLocation.latitude} - longitude {userLocation.longitude}</h4>
+        </>
+        } */}
       </div>
     </>
   )
