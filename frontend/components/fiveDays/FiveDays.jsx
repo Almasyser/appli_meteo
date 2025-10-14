4492fe437bfe86f8c7dbb59b672f8a90683c90f4
Almasyser <almasyser@wanadoo.fr>
2025-10-14 19:06:49 +0200

    interface 8eme-OneDay

--- frontend/components/fiveDays/FiveDays.jsx
+++ frontend/components/fiveDays/FiveDays.jsx
@@ -2,19 +2,17 @@ import { useEffect, useState } from "react";
 import ConvertDataJMA from "../utils/ConvertDateJMA";
 import DateToHour from "../utils/DateToHour";
 import DayOfWeek from "../utils/DayOfWeek";
-import RainDaily from "../utils/raindaily";
+import OneDay from "../oneDay/OneDay";
 import omm_codes from "../../json/omm_codes.json";
 import tags from "../../assets/tags";
 import parapluie from "../../assets/parapluie.png";
 import "./fiveDays.css";
+import OneDay from "../oneDay/OneDay";
   function FiveDays({myArray, setShowFive, showFive}){
     const [dayIndex] = useState([1,2,3,4,5,6]);
-    const [maxValue, setMaxValue] = useState(0);
-    const [maxIndex, setMaxIndex] = useState(0);
-  
     return (
       <div className={showFive? "fiveDays-box active":"fiveDays-box"}>
-        {myArray.daily && dayIndex.map((btn,index)=>{
+        {myArray.daily && dayIndex.map((btn)=>{
           return(
             <div key={btn}>
               {btn &&
@@ -23,18 +21,14 @@ import "./fiveDays.css";
                     <p className="fiveDays-dayOfWeek"><DayOfWeek today={myArray.daily.sunset[btn]}/></p>
                     <p className="fiveDays-dateJMA"><ConvertDataJMA dateISO={myArray.daily.sunset[btn]} /></p>
                   </div>
-                  <span className="fiveDays-ephemeride"><img src={tags["Soleil"]} alt="--@--"/><p><DateToHour today={myArray.daily.sunrise[btn]}/></p></span>
-                  <span className="fiveDays-ephemeride"><img src={tags["Lune"]} alt="--@--"/><p><DateToHour today={myArray.daily.sunset[btn]}/></p></span>
+                  <span className="fiveDays-ephemeride"><img src={tags["Soleil"]} alt="@"/><p><DateToHour today={myArray.daily.sunrise[btn]}/></p></span>
+                  <span className="fiveDays-ephemeride"><img src={tags["Lune"]} alt="@"/><p><DateToHour today={myArray.daily.sunset[btn]}/></p></span>
                   <p className="fiveDays-tendance">{omm_codes.filter(el => el.code === myArray.daily.weather_code[btn])[0].text}</p>
                   <img src={tags[omm_codes.filter(el => el.code === myArray.daily.weather_code[btn])[0].file]} alt="==="/>
-                  <RainDaily myArray={myArray} dayIndex={dayIndex} setMaxValue={setMaxValue} setMaxIndex={setMaxIndex}/>
-                  {(maxValue > 0)?
                   <span className="fiveDays-rain-box">
-                    <img src={parapluie} alt="^^"/>
-                    <h3>{maxValue}% vers {maxIndex} heure</h3>
+                    <img src={parapluie} alt="#" />
+                    <h4>{myArray.hourly.precipitation_probability[(btn*24)-24, btn*24]}%</h4>
                   </span>
-                  : null
-                  }
                 </div>
               }
             </div>
@@ -44,3 +38,9 @@ import "./fiveDays.css";
   }
 
   export default FiveDays;
+
+  // <RainDaily myArray={myArray} btn={btn} setMaxValue={setMaxValue} setMaxIndex={setMaxIndex}/>
+  //                 <span className="fiveDays-rain-box">
+  //                   <img src={parapluie} alt="^^"/>
+  //                   <h3>{maxValue}% vers {maxIndex} heure</h3>
+  //                 </span>
---
+++ frontend/components/oneDay/OneDay.jsx
@@ -0,0 +1,7 @@
+import "./oneday.css";
+function OneDay(){
+  return(
+    <h1>OneDay</h1>
+  )
+}
+export default OneDay;
