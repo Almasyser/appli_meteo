3aecbb88de5d68f913ac7212f0e1fffaf8d3ae2c
Almasyser <almasyser@wanadoo.fr>
2025-10-31 20:09:36 +0100

    palette

--- frontend/components/oneDay/OneDay.jsx
+++ frontend/components/oneDay/OneDay.jsx
@@ -22,35 +22,34 @@ function OneDay({btn, showOneDay, setShowOneDay}){
       </span>
       <div className="oneDay-box">
         <span className="oneDay-labels">
-        <h4>{hourlyLabels[0].label} </h4>
-        <h4>{hourlyLabels[1].label} </h4>
-        <h4>{hourlyLabels[2].label} </h4>
-        <h4>{hourlyLabels[3].label} </h4>
-        <h4>{hourlyLabels[4].label} </h4>
-        <h4>{hourlyLabels[5].label} </h4>
-        <h4>{hourlyLabels[6].label} </h4>
-        <h4>{hourlyLabels[7].label} </h4>
-        <h4>{hourlyLabels[8].label} </h4>
-        <h4>{hourlyLabels[9].label} </h4>
-        <h4>{hourlyLabels[10].label}</h4>
-        <h4>{hourlyLabels[14].label}</h4>
-        <h4>{hourlyLabels[15].label}</h4>
-        <h4>{hourlyLabels[16].label}</h4>
-        <h4>{hourlyLabels[17].label}</h4>
+          <h4>{hourlyLabels[0].label} </h4>
+          <h4>{hourlyLabels[1].label} </h4>
+          <h4>{hourlyLabels[2].label} </h4>
+          <h4>{hourlyLabels[3].label} </h4>
+          <h4>{hourlyLabels[5].label} </h4>
+          <h4>{hourlyLabels[6].label} </h4>
+          <h4>{hourlyLabels[7].label} </h4>
+          {/* <h4>{hourlyLabels[8].label} </h4> */}
+          <h4>{hourlyLabels[9].label} </h4>
+          <h4>{hourlyLabels[10].label}</h4>
+          <h4>{hourlyLabels[14].label}</h4>
+          <h4>{hourlyLabels[15].label}</h4>
+          <h4>{hourlyLabels[16].label}</h4>
+          <h4>{hourlyLabels[17].label}</h4>
         </span>
-        {intervals && intervals.map((el)=>{
+
+        {myArray && intervals && intervals.map((el)=>{
             const localId=(btn*24)-24+el; 
             return(
               <span key={el} className="oneDay-card">
                 <div><h4><DateToHour today={myArray?.hourly?.time[localId] ?? "n/a"}/></h4><h4>{hourlyLabels[0].unit}</h4></div>
                 <div><h4>{myArray?.hourly?.temperature_2m[localId]}</h4><h4>{hourlyLabels[1].unit} </h4></div>                   
                 <div><h4>{myArray?.hourly?.apparent_temperature[localId]}   </h4><h4>{hourlyLabels[2].unit} </h4></div> 
-                <img src={tags[omm_codes.filter(el => el.code === myArray.daily.weather_code[btn])[0].file]} alt="==="/>         
+                <div>{myArray.daily && <img src={tags[omm_codes.filter(el => el.code === myArray?.daily?.weather_code[btn])[0].file]} alt="==="/>}</div>
                 <div><h4>{myArray?.hourly?.showers[localId]}</h4><h4>{hourlyLabels[5].unit}</h4></div>               
                 <div><h4>{myArray?.hourly?.snowfall[localId]}</h4><h4>{hourlyLabels[6].unit}</h4></div>               
                 <div><h4>{myArray?.hourly?.snow_depth[localId]}</h4><h4>{hourlyLabels[7].unit}</h4></div> 
-                <p className="fiveDays-tendance">{omm_codes.filter(el => el.code === myArray.daily.weather_code[btn])[0].abrege}</p>              
-                {/* <div><h4>{myArray?.hourly?.weather_code[localId]}</h4><h4>{hourlyLabels[8].unit}</h4></div>                */}
+                {/* <div>{myArray.daily && <h4>{omm_codes.filter(el => el.code === myArray?.daily?.weather_code[btn])[0].text}</h4>}</div> */}
                 <div><h4>{myArray?.hourly?.surface_pressure[localId]}</h4><h4>{hourlyLabels[9].unit}</h4></div>               
                 <div><h4>{myArray?.hourly?.cloud_cover[localId]}</h4><h4>{hourlyLabels[10].unit}</h4></div>               
                 <div><h4>{myArray?.hourly?.visibility[localId]}</h4><h4>{hourlyLabels[14].unit}</h4></div>               
