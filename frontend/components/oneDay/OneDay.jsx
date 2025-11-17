e89cc6c427eba8b808de96772dc03f26409e3a7b
Almasyser <almasyser@wanadoo.fr>
2025-11-17 12:29:42 +0100

    details days: installer icones et autres

--- frontend/components/oneDay/OneDay.jsx
+++ frontend/components/oneDay/OneDay.jsx
@@ -19,39 +19,35 @@ function OneDay({btn, showOneDay, setShowOneDay}){
         <button className="btn-retour" onClick={handleClick}>Retour</button>
       </span>
       <div className="oneDays-datas">
-        <span className="oneDay-labels">
-          <h4>{hourlyLabels[0].label} </h4>
-          <h4>{hourlyLabels[1].label} </h4>
-          <h4>{hourlyLabels[2].label} </h4>
-          <h4>{hourlyLabels[3].label} </h4>
-          <h4>{hourlyLabels[5].label} </h4>
-          <h4>{hourlyLabels[6].label} </h4>
-          <h4>{hourlyLabels[7].label} </h4>
-          <h4>{hourlyLabels[9].label} </h4>
-          <h4>{hourlyLabels[10].label}</h4>
-          <h4>{hourlyLabels[14].label}</h4>
-          <h4>{hourlyLabels[15].label}</h4>
-          <h4>{hourlyLabels[16].label}</h4>
-          <h4>{hourlyLabels[17].label}</h4>
-        </span>
+
         <div className="oneDay-box">
           {myArray && intervals && intervals.map((el)=>{
             const localId=(btn*24)-24+el; 
             return(
               <span key={el} className="oneDay-card">
-                <div><h4><DateToHour today={myArray?.hourly?.time[localId] ?? "n/a"}/></h4><h4>{hourlyLabels[0].unit}</h4></div>
-                <div><h4>{myArray?.hourly?.temperature_2m[localId]}</h4><h4>{hourlyLabels[1].unit} </h4></div>                   
-                <div><h4>{myArray?.hourly?.apparent_temperature[localId]}   </h4><h4>{hourlyLabels[2].unit} </h4></div> 
-                <div><h4>{myArray?.hourly?.precipitation_probability[localId]}  </h4><h4>{hourlyLabels[3].unit} </h4></div>
-                <div><h4>{myArray?.hourly?.showers[localId]}</h4><h4>{hourlyLabels[5].unit}</h4></div>               
-                <div><h4>{myArray?.hourly?.snowfall[localId]}</h4><h4>{hourlyLabels[6].unit}</h4></div>               
-                <div><h4>{myArray?.hourly?.snow_depth[localId]}</h4><h4>{hourlyLabels[7].unit}</h4></div> 
-                <div><h4>{myArray?.hourly?.surface_pressure[localId]}</h4><h4>{hourlyLabels[9].unit}</h4></div> 
-                <div><h4>{myArray?.hourly?.cloud_cover[localId]}</h4><h4>{hourlyLabels[10].unit}</h4></div>               
-                <div><h4>{myArray?.hourly?.visibility[localId]}</h4><h4>{hourlyLabels[14].unit}</h4></div>               
-                <div><h4>{myArray?.hourly?.wind_speed_10m[localId]}</h4><h4>{hourlyLabels[15].unit}</h4></div>               
-                <div><h4>{myArray?.hourly?.wind_direction_10m[localId]}</h4><h4>{hourlyLabels[16].unit}</h4></div>               
-                <div><h4>{myArray?.hourly?.uv_index[localId]}</h4><h4>{hourlyLabels[17].unit}</h4></div>
+
+                <h4><DateToHour today={myArray?.hourly?.time[localId] ?? "n/a"}/></h4><h4>{hourlyLabels[0].unit}</h4>
+ 
+                <div>
+                  <h4>{myArray?.hourly?.temperature_2m[localId]}</h4>
+                  <h4>/</h4>
+                  <h4>{myArray?.hourly?.apparent_temperature[localId]}</h4><h4>{hourlyLabels[2].unit} </h4>
+                </div> 
+
+                <div><h4>{myArray?.hourly?.precipitation_probability[localId]}</h4><h4>{hourlyLabels[3].unit} </h4></div>
+
+                <div>
+                  <h4>{myArray?.hourly?.showers[localId]}</h4><h4>{hourlyLabels[5].unit}</h4>
+                  <h4>{myArray?.hourly?.snowfall[localId]}</h4><h4>{hourlyLabels[6].unit}</h4>          
+                  <h4>{myArray?.hourly?.snow_depth[localId]}</h4><h4>{hourlyLabels[7].unit}</h4> 
+                </div>  
+
+                <div><h4>{myArray?.hourly?.cloud_cover[localId]}</h4><h4>{hourlyLabels[10].unit}</h4></div> 
+
+                <div>
+                  <h4>{myArray?.hourly?.wind_speed_10m[localId]}</h4><h4>{hourlyLabels[15].unit}</h4>              
+                  <h4>{myArray?.hourly?.wind_direction_10m[localId]}</h4><h4>{hourlyLabels[16].unit}</h4>
+                </div>               
               </span>
             )
           })}
@@ -62,3 +58,6 @@ function OneDay({btn, showOneDay, setShowOneDay}){
 }
 export default OneDay;
 
+{/* <div><h4>{myArray?.hourly?.surface_pressure[localId]}</h4><h4>{hourlyLabels[9].unit}</h4></div>  */}
+{/* <div><h4>{myArray?.hourly?.visibility[localId]}</h4><h4>{hourlyLabels[14].unit}</h4></div>                */}
+{/* <div><h4>{myArray?.hourly?.uv_index[localId]}</h4><h4>{hourlyLabels[17].unit}</h4></div> */}
