d268bb5c4a63453c51d5b75d492a1e5973df8a22
almasyser <almasyser@wanadoo.fr>
2026-03-10 23:41:31 +0100

    erreur dans dataCharts

--- frontend/components/dataChartBox/DataChartBox.jsx
+++ frontend/components/dataChartBox/DataChartBox.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { BiLogIn } from "react-icons/bi"
+import { Await } from "react-router-dom";
 
 function DataChartBox({datas, cles}) {
-    console.log(datas.hourly);
+  const [ myArray, setMyArray] = useState([])
+  const [ show, setShow] = useState(false)
+  const handleClick = (e)=> {
+      const localKey = e.target.value;
+      setMyArray([...datas.hourly[localKey]]);
+      const moyenne =()=> myArray.reduce((a, b) => parseFloat(a) + parseFloat(b) / myArray.length);
+      console.log("MOY",moyenne());
+      setShow(true)
+      
+  }
     
-    
+
     
   return (
-    <div>DataChartBox</div>
+    <>
+      {cles && cles.map((el)=>{
+        return(
+          <div key={el}>
+            <br/>
+            <button value={el} onClick={(e)=>handleClick(e)}>{el}</button>
+          </div>
+        )}
+      )}
+      <div className="chart-box">
+        {show && myArray && myArray.map((el, index)=>{
+          return(
+            <p key={index}>{el}</p>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
-export default DataChartBox
+export default DataChartBox;
+// const moyenne = tableau.reduce((a, b) => a + b, 0) / tableau.length;.
